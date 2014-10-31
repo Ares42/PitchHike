0749b029bb46472bc77deb3abe56a451bf0b7ecf
Ares42 <luke_solomon@me.com>
2014-10-30 22:55:33 -0700

    Initial Commit!
    Who's ready for Vegas?

---
+++ PitchHike/PitchHike/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PitchHike
+//
+//  Created by Luke Solomon on 10/30/14.
+//  Copyright (c) 2014 PitchHikeinc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
