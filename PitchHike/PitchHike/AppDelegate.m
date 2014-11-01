a5888deeace8401ea9cb75e3f5619bb52c34fc0e
phillipou94 <phillipou@Phillips-MacBook-Pro.local>
2014-10-31 23:03:15 -0700

    parse added

--- PitchHike/PitchHike/AppDelegate.m
+++ PitchHike/PitchHike/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -16,7 +17,10 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    [Parse setApplicationId:@"uw4dSGN3rmYYyd8cmaG3nTVNFfq2WuaJ2MlTICtR"
+                  clientKey:@"KBkl8fuiQkx9m8KkuUoDyUf7YlKFHBsnLVT9HhW9"];
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+
     return YES;
 }
 
